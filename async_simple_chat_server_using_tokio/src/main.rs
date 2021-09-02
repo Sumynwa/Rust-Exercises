9a08c57f89933810052824d8681952e110549e48
Sumynwa <sumedh.alok@gmail.com>
2021-09-02 06:07:07 +0530

    Simple async chat server implementation using tokio crate.

---
+++ async_simple_chat_server_using_tokio/src/main.rs
@@ -0,0 +1,46 @@
+use tokio::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::sync::broadcast;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+    let (tx, _) = broadcast::channel(20);
+
+    loop {
+        let (mut socket, addr) = listener.accept().await.unwrap();
+     
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+ 
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+             let mut reader = BufReader::new(reader);
+             let mut line = String::new();
+
+             loop {
+                 tokio::select! {
+                     result = reader.read_line(&mut line) => {
+                         if result.unwrap() == 0 {
+                             break;
+                         }
+
+                         tx.send((line.clone(), addr)).unwrap();
+                         line.clear();
+                     }
+
+                     result = rx.recv() => {
+                         let (mesg, sender_addr) = result.unwrap();
+
+                         if addr != sender_addr {
+                             writer.write_all(mesg.as_bytes()).await.unwrap();
+                         }
+                     }
+                 }
+             }
+
+        });
+     }
+}
